e80b4896e2ba000f587c3ae036369656a0eb36f2
irfan babar <irfanbabar99@gmail.com>
2023-05-16 17:01:37 +0500

    handle some edge cases for recipient, bcc, cc

--- lib/whitelist_recipients/mailer_interceptor.rb
+++ lib/whitelist_recipients/mailer_interceptor.rb
@@ -2,28 +2,26 @@
 module MailerInterceptorModule
   # whitelist email addresses
   def delivering_email(message)
-    unless Rails.env.production?
-      whitelist = Rails.application.config.action_mailer.smtp_settings[:whitelist_email_addresses]
-      whitelist_cc = Rails.application.config.action_mailer.smtp_settings[:whitelist_mailer_cc]
-      whitelist_bcc = Rails.application.config.action_mailer.smtp_settings[:whitelist_mailer_bcc]
+    whitelist = Rails.application.config.action_mailer.smtp_settings[:whitelist_email_addresses]
+    whitelist_cc = Rails.application.config.action_mailer.smtp_settings[:whitelist_mailer_cc]
+    whitelist_bcc = Rails.application.config.action_mailer.smtp_settings[:whitelist_mailer_bcc]
 
-      # ignore feature if smtp_settings[:whitelist_email_addresses] not set
-      unless whitelist.nil?
-        final_whitelist = message.to.select {|receiver| whitelist.include? receiver }
-        message.to = final_whitelist
-        if final_whitelist.blank?
-          message.to = []
-          message.perform_deliveries = false # stop sending mail
-        end
-        unless whitelist_cc.nil?
-          final_whitelist_cc = message.cc.select {|receiver| whitelist_cc.include? receiver}
-          message.cc = final_whitelist_cc
-        end
-        unless whitelist_bcc.nil?
-          final_whitelist_bcc = message.bcc.select {|receiver| whitelist_bcc.include? receiver}
-          message.bcc = final_whitelist_bcc
-        end
+    # ignore feature if smtp_settings[:whitelist_email_addresses] not set
+    if !whitelist.nil? and message.to.present?
+      final_whitelist = message.to.select {|receiver| whitelist.include? receiver }
+      message.to = final_whitelist
+      if final_whitelist.blank?
+        message.to = []
+        message.perform_deliveries = false # stop sending mail
       end
+    end
+    if !whitelist_cc.nil? and message.cc.present? and message.perform_deliveries
+      final_whitelist_cc = message.cc.select {|receiver| whitelist_cc.include? receiver}
+      message.cc = final_whitelist_cc
+    end
+    if !whitelist_bcc.nil? and message.bcc.present? and message.perform_deliveries
+      final_whitelist_bcc = message.bcc.select {|receiver| whitelist_bcc.include? receiver}
+      message.bcc = final_whitelist_bcc
     end
   end
 end
